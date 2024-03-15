ed8965ab9e72307782e99f86bf9ece0d5ab40721
bennfsx <bennfsx@gmail.com>
2024-03-16 03:20:11 +0800

    Add on parameter

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,6 +9,8 @@ const Form = (props) => {
   const [annualAmount, setAnnualAmount] = useState(null);
   const [monthlyAmount, setMonthlyAmount] = useState(null);
   const [isCreditSalaryValid, setIsCreditSalaryValid] = useState(null);
+  const [salaryBonus, setSalaryBonus] = useState(null);
+  const [uniqueBillPayments, setUniqueBillPayments] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +18,8 @@ const Form = (props) => {
         let url = `https://api.ocbc.com:8243/OCBC360_Interest/1.0?accountBalance=${accountBalance}`;
         if (isCreditSalaryValid)  {
           url += `&salary=true`;
+        } else if (isCreditSalaryValid && uniqueBillPayments){
+          url += `&salary=true&payBill=true`;
         }
         const response = await fetch(url, {
           headers: {
@@ -27,6 +31,7 @@ const Form = (props) => {
           const data = await response.json();
           setBaseInterest(data.results.component.baseInterest);
           setAnnualAmount(data.results.totalYearly);
+          setSalaryBonus(data.results.component.salaryBonus);
           setMonthlyAmount(data.results.totalMonthly);
         } else {
           console.error("Failed to fetch data:", response.statusText);
@@ -37,7 +42,7 @@ const Form = (props) => {
     };
   
     fetchData();
-  }, [accountBalance, isCreditSalaryValid]);
+  }, [accountBalance, isCreditSalaryValid, uniqueBillPayments]);
   
   
 
@@ -46,11 +51,11 @@ const Form = (props) => {
     setAccountBalance(e.target.value);
   };
 
-  const interestRate = (baseInterest / accountBalance) * 100;
+  const interestRate = ((annualAmount) / accountBalance) * 100;
 
   const handleCreditSalaryCheck = (e) => {
     const creditSalary = parseFloat(e.target.value);
-    if (!isNaN(creditSalary) && creditSalary > 1800) {
+    if (!isNaN(creditSalary) && creditSalary >= 1800) {
       setIsCreditSalaryValid(true);
       console.log(creditSalary)
     } else {
@@ -58,11 +63,18 @@ const Form = (props) => {
       console.log(creditSalary)
     }
   };
+
+  const handleUniqueBillPaymentCheck = (value) =>{
+    setUniqueBillPayments((prevValue) => prevValue === null ? value : !prevValue);
+
+    console.log(uniqueBillPayments);
+    }
+  
   
   return (
     <div className={styles.formContainer}>
       <h1 className={styles.formTitle}>Savings Interest Calculator</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
         <label className={styles.label}>
           Account Balance
           <input
@@ -83,13 +95,9 @@ const Form = (props) => {
           />
         </label>
         <label className={styles.label}>
-          Number of bills Paid
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="3"
-            // onChange={handleCreditSalaryCheck}
-          />
+          Made at least 3 unique bill payments?
+          <button onClick={() => handleUniqueBillPaymentCheck(false)}>Yes</button>
+          <button onClick={() => handleUniqueBillPaymentCheck(true)}>No</button>
         </label>
         <label className={styles.label}>
           Interest Rate &nbsp;
@@ -97,7 +105,7 @@ const Form = (props) => {
             type="text"
             className={styles.inputInterest}
             readOnly
-            value={interestRate !== null ? interestRate + "%" : "Loading..."}
+            value={interestRate !== null ? interestRate.toFixed(4) + "%" : "Loading..."}
           />
         </label>
       </form>
